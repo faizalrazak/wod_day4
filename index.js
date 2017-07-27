b4fee764ea029cc97884637b791fe34bacc6523f
faizalrazak <mohamadfaizalrazak@gmail.com>
2017-07-27 14:55:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function timeConvert(inputTime){
+  var split = inputTime.split(":");
+  var first = parseInt(split[0]);
+  var ampm = inputTime.substring(8,10);
+ 
+  if(ampm == "PM"){
+   document.write(first + ":" + split[1] + ":" + inputTime[6]+inputTime[7]);
+  }
+  else{
+    first = "00";
+    document.write(first+":"+split[1]+":"+inputTime[6]+inputTime[7]);
+  } 
+}
+timeConvert("12:30:34PM");
